40802fe1378ead41c262b6fe090bc2892fab8353
Diego Montalti <diego.montalti@studio.unibo.it>
2025-11-06 15:29:32 +0100

    Completing exercises.

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,7 +1,6 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -11,11 +10,75 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
+    }
+
+    private enum Month {
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(int days) {
+            this.days = days;
+        }
+
+        public static Month fromString(String value) {
+            Objects.requireNonNull(value);
+            Month result = null;
+
+            try {
+                result = valueOf(value);
+                return result;
+            } catch (IllegalArgumentException e) {
+                result = null;
+                for(Month elem : Month.values()) {
+                    if(elem.toString().toLowerCase().contains(value.toLowerCase())) {
+                        if(result != null) {
+                            throw new IllegalArgumentException("Ambiguos result for the string in input ( "+value+" )");
+                        }
+                        result = elem;
+                    }
+                }
+            
+                if(result == null) {
+                    throw new IllegalArgumentException("The string in input ( "+value+" ) was not a Month");
+                }
+
+            return result;
+            }
+            
+        }
+    }
+
+    private class SortByMonthOrder implements Comparator<String> {
+        @Override
+        public int compare(String arg0, String arg1) {
+            return Month.fromString(arg0).compareTo(Month.fromString(arg1));
+        }
+    }
+
+    private class SortByDate implements Comparator<String> {
+        @Override
+        public int compare(String arg0, String arg1) {
+            return Integer.compare(Month.fromString(arg0).days,
+                                   Month.fromString(arg1).days );
+        }
+        
     }
 }
